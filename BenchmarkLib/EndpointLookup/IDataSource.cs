459c58eb9ccc9172cf76f252da5b6c6619fd0c17
Vedant Thakkar <vethakkar@microsoft.com>
2024-01-11 11:17:36 +0530

    Bug fix

--- BenchmarkLib/EndpointLookup/IDataSource.cs
+++ BenchmarkLib/EndpointLookup/IDataSource.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BenchmarkDotNet.Attributes; // Added missing using statement
 
 namespace BenchmarkLib.EndpointLookup
 {
